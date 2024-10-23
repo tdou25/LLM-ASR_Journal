e15043e6544e07a5c2a4686406a70860fc66f2b6
tdou25 <thomasyd1120@gmail.com>
2024-10-22 21:55:31 -0400

    modified a line

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     model = whisper.load_model("base")
     
     # Expand the file path
-    file_path = os.path.expanduser("/Users/tdou/Documents/RCOS_workspace/ASR_Enhancement_LLM/LibriSpeech/test-clean/61/70968/61-70968-0000.flac")
+    file_path = os.path.expanduser("/Users/tdou/Documents/RCOS_workspace/LLM-ASR_Journal/LibriSpeech/test-clean/61/70968/61-70968-0000.flac")
     
     result = model.transcribe(file_path)
     print(f' The text in video: \n {result["text"]}')
